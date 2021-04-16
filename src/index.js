ed4e6ca3b61110192a245a0d7ce34e1ca968a9fc
unknown <tjtik83@gmail.com>
2021-04-16 18:07:18 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Person = ({ img, name, job, children }) => {
+
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
+  return (
+    <article className="person">
+      <img src={url} />
+      <h3>Name: {name}</h3>
+      <h4>Job: {job}</h4>
+      {children}
+    </article>
+  )
+}
+
+const PeopleList = () => {
+  return (
+    <section className="person-list">
+      <Person img="1" name="TIKK1" job="Programmer1">
+        <p>Skill : C# JAVA PHP</p>
+      </Person>
+      <Person img="2" name="TIKK2" job="Programmer2" >
+        <p>Skill : SQL</p>
+      </Person>
+      <Person img="3" name="TIKK3" job="Programmer3" >
+        <p>Skill : JAVASCRIPT</p>
+      </Person>
+      <Person img="4" name="TIKK4" job="Programmer4" >
+        <p>Skill : PYTHON</p>
+      </Person>
+      <Person img="1" name="TIKK1" job="Programmer1">
+        <p>Skill : C# JAVA PHP</p>
+      </Person>
+      <Person img="2" name="TIKK2" job="Programmer2" >
+        <p>Skill : SQL</p>
+      </Person>
+      <Person img="3" name="TIKK3" job="Programmer3" >
+        <p>Skill : JAVASCRIPT</p>
+      </Person>
+      <Person img="4" name="TIKK4" job="Programmer4" >
+        <p>Skill : PYTHON</p>
+      </Person>
+    </section>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <PeopleList />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
